709b7abdf3ff4378865810349ba8857789ea1477
Viet Nguyen <>
2023-02-08 14:46:02 +0700

    10. Refactoring Input Container View For Reusability

--- TwitterTutorial/Controller/Authentication/LoginController.swift
+++ TwitterTutorial/Controller/Authentication/LoginController.swift
@@ -20,29 +20,28 @@ class LoginController: UIViewController {
     }()
     
     private lazy var emailContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .systemPurple
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let image = UIImage(named: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image!, textField: emailTextField)
         
-        let iv = UIImageView()
-        iv.image = UIImage(named: "mail")
-        view.addSubview(iv)
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
         return view
     }()
     
     private lazy var passwordContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let image = UIImage(named: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image!, textField: passwordTextField)
         
-        let iv = UIImageView()
-        iv.image = UIImage(named: "ic_lock_outline_white_2x")
-        view.addSubview(iv)
-        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
-        iv.setDimensions(width: 24, height: 24)
         return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
     }()
     
     private lazy var buttonLoginContainerView: UIView = {
@@ -80,6 +79,6 @@ class LoginController: UIViewController {
         stack.spacing = 8
         
         view.addSubview(stack)
-        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 16, paddingRight: 16)
     }
 }
