ca1cd3c8be5c857410f95639cf8838893a0ba008
Viet Nguyen <>
2023-02-13 00:12:49 +0700

    28. Adding Caption TextView

--- TwitterTutorial/Controller/UploadTweetController.swift
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -38,6 +38,8 @@ class UploadTweetController: UIViewController {
         iv.backgroundColor = .twitterBlue
         return iv
     }()
+    
+    private let captionTextView = CaptionTextView()
         
     // MARK: - Lifecycle
     
@@ -76,12 +78,19 @@ class UploadTweetController: UIViewController {
         view.backgroundColor = .white
         configureNavigationBar()
         
-        view.addSubview(profileImageView)
-        profileImageView.anchor(
+        let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+        stack.axis = .horizontal
+        stack.spacing = 12
+        
+        view.addSubview(stack)
+        stack.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
             left: view.leftAnchor,
+            right: view.rightAnchor,
             paddingTop: 16,
-            paddingLeft: 16)
+            paddingLeft: 16,
+            paddingRight: 16
+        )
         
         profileImageView.sd_setImage(with: user.profileImageUrl)
         
