69d06c8ef8c440e575e97b13edd1c60bc157d2f9
Viet Nguyen <>
2023-03-25 19:42:10 +0700

    63. Use ViewModel To Populate Data

--- TwitterTutorial/Controller/UploadTweetController.swift
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -41,6 +41,15 @@ class UploadTweetController: UIViewController {
         return iv
     }()
     
+    private lazy var replyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "replying to @spiderman"
+        label.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        return label
+    }()
+    
     private let captionTextView = CaptionTextView()
         
     // MARK: - Lifecycle
@@ -98,10 +107,19 @@ class UploadTweetController: UIViewController {
         view.backgroundColor = .white
         configureNavigationBar()
         
-        let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
-        stack.axis = .horizontal
+        let imageCaptionStack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+        imageCaptionStack.axis = .horizontal
+        imageCaptionStack.spacing = 12
+        imageCaptionStack.alignment = .leading
+        
+        let stack = UIStackView(arrangedSubviews: [replyLabel, imageCaptionStack])
+        stack.axis = .vertical
+        // stack.alignment = .leading
         stack.spacing = 12
-        stack.alignment = .leading
+        
+        view.addSubview(stack)
+        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor,
+                     right: view.rightAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16)
         
         view.addSubview(stack)
         stack.anchor(
@@ -114,6 +132,16 @@ class UploadTweetController: UIViewController {
         )
         
         profileImageView.sd_setImage(with: user.profileImageUrl)
+        
+        actionButton.setTitle(viewModel.actionButtonTitle, for: .normal)
+        captionTextView.placeholderLabel.text = viewModel.placeholderText
+        
+        replyLabel.isHidden = !viewModel.shouldShowReplyLabel
+        guard let replyText = viewModel.replyText else { return }
+        replyLabel.text = replyText
+    }
+    
+    func configureUIComponents() {
         
     }
     
